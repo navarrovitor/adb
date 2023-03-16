44027aee562c5d156b99c49f9339e964129410c7
Vitor Navarro <vitor.san.nav.nexus@gmail.com>
2023-03-16 11:46:23 -0300

    add caracal gem to write docx files

---
+++ Gemfile
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# gem "rails"
+gem 'caracal'
+gem "ruby-lsp", "~> 0.4.1", :group => :development, :require => false
--- file_reader.rb
+++ file_reader.rb
@@ -1,13 +1,32 @@
-require 'csv'
-require 'fileutils'
+require "csv"
+require "fileutils"
+require "caracal"
 
 # Empty output directory
-FileUtils.rm_rf("output/.", secure: true)
+# FileUtils.rm_rf("output/.", secure: true)
 
-files = %w[alagoas catimbau ceara inaja]
+# Put all the input file names so it can iterate through all
+files = %w[inaja]
 
 files.each do |file|
-    CSV.foreach("input/#{file}.csv", headers: :first_row) do |row|
-        
+  Caracal::Document.save "output/#{file}.docx" do |document|
+    document.style do
+      id "Heading1" # sets the internal identifier for the style.
+      name "heading 1" # sets the friendly name of the style.
+      type "paragraph" # sets the style type. accepts `paragraph` or `character`
+      font "Arial" # sets the font family.
+      size 28 # sets the font size. units in half points.
+      bold true # sets the font weight.
     end
+
+    CSV.foreach(
+      "input/#{file}.csv",
+      headers: :first_row,
+      encoding: "bom|utf-8"
+    ) do |row|
+      new_line = ->(field) { field + ": #{row[field].upcase}" }
+      document.h1 "New Page"
+      document.page
+    end
+  end
 end
